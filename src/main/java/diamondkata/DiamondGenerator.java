a3dba016721f9bce6b00999ae358b538c320d0f4
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2015-01-16 12:18:14 +0000

    3 made the test pass:  acceptance test (DiamondTest) is now broken but that's fine - we have not finished the task yet

--- src/main/java/diamondkata/DiamondGenerator.java
+++ src/main/java/diamondkata/DiamondGenerator.java
@@ -7,15 +7,7 @@ import java.util.List;
 public class DiamondGenerator {
 
     public List<String> generate(char c) {
-        return ImmutableList.of(
-                "   A   ",
-                "  B B  ",
-                " C   C ",
-                "D     D",
-                " C   C ",
-                "  B B  ",
-                "   A   "
-        );
+        return ImmutableList.of("A");
     }
 
 }
