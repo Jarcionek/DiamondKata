330506c764db8efc0ffbfc37c58ada2f11ec0e4a
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2015-01-16 12:03:24 +0000

    1 made the test compile

---
+++ src/main/java/diamondkata/Diamond.java
@@ -0,0 +1,15 @@
+package diamondkata;
+
+import java.util.List;
+
+public class Diamond {
+
+    public static Diamond newDiamond(char c) {
+        return null;
+    }
+
+    public List<String> asListOfRows() {
+        return null;
+    }
+
+}
