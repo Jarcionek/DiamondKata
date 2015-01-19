3e234de82bacb80c808829346447b17e092bcbdd
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2015-01-19 16:43:22 +0000

    reimplemented

--- src/main/java/diamondkata/Diamond.java
+++ src/main/java/diamondkata/Diamond.java
@@ -2,22 +2,39 @@ package diamondkata;
 
 import java.util.List;
 
+import static java.lang.Math.abs;
+import static java.util.Arrays.fill;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
+
 public class Diamond {
 
-    private static final DiamondGenerator DIAMOND_GENERATOR = new DiamondGenerator(new DiamondRowGenerator());
+    private final char[][] chars;
 
-    private final List<String> rows;
-
-    private Diamond(List<String> rows) {
-        this.rows = rows;
+    private Diamond(char[][] chars) {
+        this.chars = chars;
     }
 
-    public static Diamond newDiamond(char c) {
-        return new Diamond(DIAMOND_GENERATOR.generate(c));
+    public static Diamond newDiamond(char character) {
+        int c = character - 'A';
+        int size = c * 2 + 1;
+
+        char[][] chars = new char[size][size];
+        stream(chars).forEach(row -> fill(row, ' '));
+
+        for (int dx = -c; dx <= c; dx++) {
+            for (int dy = -c; dy <= c; dy++) {
+                if (abs(dx) + abs(dy) == c) {
+                    chars[c + dx][c + dy] =  (char) (character - c + abs(dy));
+                }
+            }
+        }
+
+        return new Diamond(chars);
     }
 
     public List<String> asListOfRows() {
-        return rows;
+        return stream(chars).map(String::new).collect(toList());
     }
 
 }
